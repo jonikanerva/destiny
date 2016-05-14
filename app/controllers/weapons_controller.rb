3d812c65192fa53a2b3bfd9b20901c8a81dbccf0
Joni Kanerva <joni@devlab.fi>
2016-05-14 23:14:08 +0300

    sort logic

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,5 +1,20 @@
 class WeaponsController < ApplicationController
   def index
-    @weapons = Item.weapons.includes(:values).order(:item_type_name)
+    @weapons = Item.weapons.includes(:values).order(sort_order)
+  end
+
+  private
+
+  def sort_order
+    case params[:sort]
+    when 'name'
+      :name
+    when 'tier'
+      :tier_type_name
+    when 'type'
+      :item_type_name
+    else
+      :item_type_name
+    end
   end
 end
