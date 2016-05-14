913c5d48a3dd007b0b3a6dac89c7baad25db52dd
Joni Kanerva <joni@devlab.fi>
2016-05-14 19:23:24 +0300

    save items

--- app/jobs/fetch_manifest_job.rb
+++ app/jobs/fetch_manifest_job.rb
@@ -8,11 +8,25 @@ class FetchManifestJob < ActiveJob::Base
       db.execute("select * from DestinyStatDefinition") do |row|
         json = JSON.parse row.second
 
-        stat = Stat.find_or_initialize_by(stat_id: json["statHash"])
-        stat.stat_id = json["statHash"]
+        stat = Stat.find_or_initialize_by(stat_hash: json["statHash"])
+        stat.stat_hash = json["statHash"]
         stat.name = json["statName"]
         stat.description = json["statDescription"]
         stat.save!
+      end
+
+      db.execute("select * from DestinyInventoryItemDefinition") do |row|
+        json = JSON.parse row.second
+
+        item = Item.find_or_initialize_by(item_hash: json["itemHash"])
+        item.item_hash      = json["itemHash"]
+        item.name           = json["itemName"]
+        item.description    = json["itemDescription"]
+        item.icon           = json["icon"]
+        item.tier_type      = json["tierType"]
+        item.tier_type_name = json["tierTypeName"]
+        item.item_type_name = json["itemTypeName"]
+        item.save!
       end
     end
   end
