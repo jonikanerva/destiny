8d04d1829c71947fb2735d9467d80750b3b0e3ef
Joni Kanerva <joni@devlab.fi>
2016-05-15 00:32:25 +0300

    reverse sort

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -5,45 +5,50 @@ class WeaponsController < ApplicationController
 
   private
 
-  def sort_order
-    case params[:sort]
-    when 'name'
-      :name
-    when 'description'
-      :description
-    when 'type'
-      :category
-    when 'tier'
-      :tier
-    when 'icon'
-      :icon
-    when 'attack'
-      :attack
-    when 'optics'
-      :optics
-    when 'rof'
-      :rate_of_fire
-    when 'impact'
-      :impact
-    when 'range'
-      :range
-    when 'stability'
-      :stability
-    when 'mag'
-      :magazine
-    when 'reload'
-      :reload_speed
-    when 'inventory'
-      :inventory_size
-    when 'equip'
-      :equip_speed
-    when 'aa'
-      :aim_assistance
-    when 'recoil'
-      :recoil_direction
-    else
-      :category
+    def sort_order
+      field = case params[:sort]
+      when 'name'
+        :name
+      when 'description'
+        :description
+      when 'type'
+        :category
+      when 'tier'
+        :tier
+      when 'icon'
+        :icon
+      when 'attack'
+        :attack
+      when 'optics'
+        :optics
+      when 'rof'
+        :rate_of_fire
+      when 'impact'
+        :impact
+      when 'range'
+        'weapons.range'
+      when 'stability'
+        :stability
+      when 'mag'
+        :magazine
+      when 'reload'
+        :reload_speed
+      when 'inventory'
+        :inventory_size
+      when 'equip'
+        :equip_speed
+      when 'aa'
+        :aim_assistance
+      when 'recoil'
+        :recoil_direction
+      else
+        :attack
+      end
+
+      "#{field} #{sort_direction}"
     end
 
-  end
+    def sort_direction
+      params[:order] == 'asc' ? :asc : :desc
+    end
 end
--- app/helpers/weapons_helper.rb
+++ app/helpers/weapons_helper.rb
@@ -6,6 +6,21 @@ module WeaponsHelper
   end
 
   def sort_link(column_name)
-    link_to column_name, { sort: column_name.downcase }, class: 'sort-link'
+    name = column_name.downcase
+
+    params = {
+      order: sort_order(name),
+      sort: name,
+    }
+
+    link_to column_name, params, class: 'sort-link'
+  end
+
+  def sort_order(column_name)
+    if params[:order] == 'desc' && params[:sort] == column_name
+      :asc
+    else
+      :desc
+    end
   end
 end
