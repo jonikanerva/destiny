cdffd2167994be2fcc223dd036c891b52cfab827
Joni Kanerva <joni@devlab.fi>
2016-05-14 23:07:09 +0300

    add sort link

--- app/helpers/weapons_helper.rb
+++ app/helpers/weapons_helper.rb
@@ -2,6 +2,10 @@ module WeaponsHelper
   def armory_link(name, item_id)
     link = "https://www.bungie.net/en/Armory/Detail?item=#{item_id}"
 
-    link_to name, link
+    link_to name, link, class: 'armory-link'
+  end
+
+  def sort_link(column_name)
+    link_to column_name, { sort: column_name.downcase }, class: 'sort-link'
   end
 end
