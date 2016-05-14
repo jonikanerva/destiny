121fe470cb4b845d7cf16fb33830bf632147edb4
Joni Kanerva <joni@devlab.fi>
2016-05-14 20:11:20 +0300

    update stat values

--- app/jobs/fetch_manifest_job.rb
+++ app/jobs/fetch_manifest_job.rb
@@ -5,20 +5,23 @@ class FetchManifestJob < ActiveJob::Base
     db_file = "/Users/joni/Desktop/world_sql_content_05feb511d99733ad8d1bbe60465007c1.content"
 
     SQLite3::Database.new(db_file) do |db|
+      puts "Updating stats"
       db.execute("select * from DestinyStatDefinition") do |row|
         json = JSON.parse row.second
 
-        stat = Stat.find_or_initialize_by(stat_hash: json["statHash"])
-        stat.stat_hash = json["statHash"]
-        stat.name = json["statName"]
+        stat = Stat.find_or_initialize_by stat_hash: json["statHash"]
+        stat.stat_hash   = json["statHash"]
+        stat.name        = json["statName"]
         stat.description = json["statDescription"]
         stat.save!
       end
 
+      puts "Updating items"
       db.execute("select * from DestinyInventoryItemDefinition") do |row|
+
         json = JSON.parse row.second
 
-        item = Item.find_or_initialize_by(item_hash: json["itemHash"])
+        item = Item.find_or_initialize_by item_hash: json["itemHash"]
         item.item_hash      = json["itemHash"]
         item.name           = json["itemName"]
         item.description    = json["itemDescription"]
@@ -27,6 +30,17 @@ class FetchManifestJob < ActiveJob::Base
         item.tier_type_name = json["tierTypeName"]
         item.item_type_name = json["itemTypeName"]
         item.save!
+
+        # loop item stat values
+        json["stats"].each do |v|
+          hash = v.last
+          value = item.values.find_or_initialize_by stat_hash: hash["statHash"]
+          value.stat_hash     = hash["statHash"]
+          value.value         = hash["value"]
+          value.minimum_value = hash["minimum"]
+          value.maximum_value = hash["maximum"]
+          value.save!
+        end
       end
     end
   end
