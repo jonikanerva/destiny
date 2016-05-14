00dffe718e1ff5cc8127527674ee60c45d183316
Joni Kanerva <joni@devlab.fi>
2016-05-14 22:48:42 +0300

    add scopes and some values

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,11 +19,67 @@ class Item < ActiveRecord::Base
   HEAVY = [
     "Machine Gun",
     "Rocket Launcher",
-    "Sword",
   ]
 
-  scope :primary,   -> { where(item_type_name: PRIMARY ) }
-  scope :secondary, -> { where(item_type_name: SECONDARY ) }
-  scope :heavy,     -> { where(item_type_name: HEAVY ) }
-  scope :weapons,   -> { where(item_type_name: PRIMARY + SECONDARY + HEAVY ) }
+  scope :active,    -> { where.not(name: ['', 'Reforge Weapon']) }
+  scope :primary,   -> { active.where(item_type_name: PRIMARY) }
+  scope :secondary, -> { active.where(item_type_name: SECONDARY) }
+  scope :heavy,     -> { active.where(item_type_name: HEAVY) }
+  scope :weapons,   -> { active.where(item_type_name: PRIMARY + SECONDARY + HEAVY) }
+
+  def attack
+    value_by_name 'Attack'
+  end
+
+  def optics
+    value_by_name 'Optics'
+  end
+
+  def rate_of_fire
+    value_by_name 'Rate of Fire'
+  end
+
+  def impact
+    value_by_name 'Impact'
+  end
+
+  def range
+    value_by_name 'Range'
+  end
+
+  def stability
+    value_by_name 'Stability'
+  end
+
+  def magazine
+    value_by_name 'Magazine'
+  end
+
+  def reload
+    value_by_name 'Reload'
+  end
+
+  def inventory_size
+    value_by_name 'Inventory Size'
+  end
+
+  def equip_speed
+    value_by_name 'Equip Speed'
+  end
+
+  def aim_assistance
+    value_by_name 'Aim assistance'
+  end
+
+  def recoil_direction
+    value_by_name 'Recoil direction'
+  end
+
+  private
+
+    def value_by_name(name)
+      value_array = self.values.to_a
+
+      value_array.keep_if { |w| w.stat_name == name }.first.try(:value)
+    end
 end
