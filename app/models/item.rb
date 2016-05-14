f328efd8e26342d5c718fac529b3e8d865d3c56a
Joni Kanerva <joni@devlab.fi>
2016-05-14 21:20:13 +0300

    add scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,29 @@
 class Item < ActiveRecord::Base
   has_many :values
   has_many :stats, through: :values
+
+  PRIMARY = [
+    "Auto Rifle",
+    "Hand Cannon",
+    "Pulse Rifle",
+    "Scout Rifle",
+  ]
+
+  SECONDARY = [
+    "Fusion Rifle",
+    "Shotgun",
+    "Sidearm",
+    "Sniper Rifle",
+  ]
+
+  HEAVY = [
+    "Machine Gun",
+    "Rocket Launcher",
+    "Sword",
+  ]
+
+  scope :primary,   -> { where(item_type_name: PRIMARY ) }
+  scope :secondary, -> { where(item_type_name: SECONDARY ) }
+  scope :heavy,     -> { where(item_type_name: HEAVY ) }
+  scope :weapons,   -> { where(item_type_name: PRIMARY + SECONDARY + HEAVY ) }
 end
