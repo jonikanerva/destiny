045e0d5792ac9d62f201f6778132a7a546e6ed24
Joni Kanerva <joni@devlab.fi>
2016-05-21 08:44:59 +0300

    remove unused

--- app/helpers/weapons_helper.rb
+++ app/helpers/weapons_helper.rb
@@ -2,22 +2,22 @@ module WeaponsHelper
   def weapon_type_options(selected)
     options = {
       'Primary' => [
-        [ "Auto Rifle",  :auto_rifle  ],
-        [ "Hand Cannon", :hand_cannon ],
-        [ "Pulse Rifle", :pulse_rifle ],
-        [ "Scout Rifle", :scout_rifle ],
+        ['Auto Rifle',  :auto_rifle ],
+        ['Hand Cannon', :hand_cannon],
+        ['Pulse Rifle', :pulse_rifle],
+        ['Scout Rifle', :scout_rifle],
       ],
 
       'Secondary' => [
-        ["Fusion Rifle", :fusion_rifle ],
-        ["Shotgun",      :shotgun      ],
-        ["Sidearm",      :sidearm      ],
-        ["Sniper Rifle", :sniper_rifle ],
+        ['Fusion Rifle', :fusion_rifle],
+        ['Shotgun',      :shotgun     ],
+        ['Sidearm',      :sidearm     ],
+        ['Sniper Rifle', :sniper_rifle],
       ],
 
       'Heavy' => [
-        [ "Machine Gun",     :machine_gun     ],
-        [ "Rocket Launcher", :rocket_launcher ],
+        ['Machine Gun',     :machine_gun    ],
+        ['Rocket Launcher', :rocket_launcher],
       ],
     }
 
@@ -28,34 +28,6 @@ module WeaponsHelper
     link = "https://www.bungie.net/en/Armory/Detail?item=#{weapon.item_hash}"
 
     link_to weapon.name, link, class: 'armory-link', title: weapon.description
-  end
-
-  def sort_link(column_name, tooltip = '')
-    name = column_name.downcase
-
-    params = {
-      order: sort_order(name),
-      sort: name,
-      type: type_select,
-    }
-
-    link_to column_name, params, class: 'sort-link', title: tooltip
-  end
-
-  def sort_order(column_name)
-    if params[:order] == 'desc' && params[:sort] == column_name
-      :asc
-    else
-      :desc
-    end
-  end
-
-  def type_select
-    if params && params[:type]
-      params[:type]
-    else
-      'auto_rifle'
-    end
   end
 
   def cache_key_for_weapons
