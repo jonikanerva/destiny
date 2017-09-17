d7182e07c4be55fed9ade4783459dd207390de6e
Joni Kanerva <jhonny@jhonny.org>
2017-09-17 16:08:07 +0300

    fetch from items

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -6,39 +6,11 @@ class WeaponsController < ApplicationController
   private
 
     def weapon_types
-      case params[:type]
-      when 'auto_rifles'
+      if params[:type]
         cookies[:weapon_type] = params[:type]
-        Weapon.auto_rifles
-      when 'hand_cannons'
-        cookies[:weapon_type] = params[:type]
-        Weapon.hand_cannons
-      when 'pulse_rifles'
-        cookies[:weapon_type] = params[:type]
-        Weapon.pulse_rifles
-      when 'scout_rifles'
-        cookies[:weapon_type] = params[:type]
-        Weapon.scout_rifles
-      when 'fusion_rifles'
-        cookies[:weapon_type] = params[:type]
-        Weapon.fusion_rifles
-      when 'shotguns'
-        cookies[:weapon_type] = params[:type]
-        Weapon.shotguns
-      when 'sidearms'
-        cookies[:weapon_type] = params[:type]
-        Weapon.sidearms
-      when 'sniper_rifles'
-        cookies[:weapon_type] = params[:type]
-        Weapon.sniper_rifles
-      when 'machine_guns'
-        cookies[:weapon_type] = params[:type]
-        Weapon.machine_guns
-      when 'rocket_launchers'
-        cookies[:weapon_type] = params[:type]
-        Weapon.rocket_launchers
+        Item.includes(:values, :stats).where(item_type: params[:type])
       else
-        type = cookies[:weapon_type] || :auto_rifles
+        type = cookies[:weapon_type] || 'auto-rifles'
         redirect_to weapons_path(type)
       end
     end
