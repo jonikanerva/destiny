f3b3008a93212d73708f93cd857dda5254900b21
Joni Kanerva <jhonny@jhonny.org>
2021-01-03 11:30:37 +0200

    sortable columns

--- src/Weapons.tsx
+++ src/Weapons.tsx
@@ -1,6 +1,6 @@
 import './Weapons.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { WeaponData } from './App'
 
 interface WeaponProps {
@@ -8,14 +8,37 @@ interface WeaponProps {
   weaponType: string
 }
 
+const sortNumeric = (field: string, direction = 'asc') => (
+  a: Record<string, number>,
+  b: Record<string, number>
+) => {
+  const multiply = direction === 'desc' ? -1 : 1
+
+  return a[field] > b[field]
+    ? -1 * multiply
+    : b[field] > a[field]
+    ? 1 * multiply
+    : 0
+}
+
 const pickWeapon = (weapons: WeaponData, type: string): Record<string, any> =>
   weapons.filter((weapon) => weapon.weaponType === type)[0]
 
 const Weapons: React.FC<WeaponProps> = ({ weaponData, weaponType }) => {
   const { columns, data } = pickWeapon(weaponData, weaponType)
+  const [sort, setSort] = useState<string>('')
+  const [sortDirection, setSortDirection] = useState<string>('asc')
 
   if (columns === undefined || data === undefined) {
     return <div>No data..</div>
+  }
+
+  const sortClick = (field: string) => {
+    const next = sortDirection === 'asc' ? 'desc' : 'asc'
+    const direction = field !== sort ? 'asc' : next
+
+    setSort(field)
+    setSortDirection(direction)
   }
 
   return (
@@ -23,34 +46,43 @@ const Weapons: React.FC<WeaponProps> = ({ weaponData, weaponType }) => {
       <table className="center">
         <thead>
           <tr>
-            {columns.map((header: any, key: number) => (
-              <th key={key}>{header.title}</th>
-            ))}
+            {columns.map((header: any, key: number) => {
+              const onClick = header.sortable
+                ? () => sortClick(header.field)
+                : undefined
+              return (
+                <th key={key} onClick={onClick}>
+                  {header.title}
+                </th>
+              )
+            })}
           </tr>
         </thead>
         <tbody>
-          {data.map((weapon: any, key: number) => (
-            <tr
-              key={`${weapon.type}${key}`}
-              className={key % 2 ? 'even' : 'odd'}
-            >
-              {columns.map((column: any, key: number) => {
-                if (column.field === 'icon') {
+          {data
+            .sort(sortNumeric(sort, sortDirection))
+            .map((weapon: any, key: number) => (
+              <tr
+                key={`${weapon.type}${key}`}
+                className={key % 2 ? 'even' : 'odd'}
+              >
+                {columns.map((column: any, key: number) => {
+                  if (column.field === 'icon') {
+                    return (
+                      <td key={key} className="image">
+                        <img height={70} src={weapon[column.field]} />
+                      </td>
+                    )
+                  }
+
                   return (
-                    <td key={key} className="image">
-                      <img height={70} src={weapon[column.field]} />
+                    <td key={key} width={column.width}>
+                      {weapon[column.field]}
                     </td>
                   )
-                }
-
-                return (
-                  <td key={key} width={column.width}>
-                    {weapon[column.field]}
-                  </td>
-                )
-              })}
-            </tr>
-          ))}
+                })}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
